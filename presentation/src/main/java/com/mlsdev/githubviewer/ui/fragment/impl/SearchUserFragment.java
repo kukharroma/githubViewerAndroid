c2768c31390fe0f10a20d6f7493a479ede0bb700
Roman Kukhar <kukharroma@gmail.com>
2015-05-26 12:14:06 +0300

    [ui] change item project layout

--- presentation/src/main/java/com/mlsdev/githubviewer/ui/fragment/impl/SearchUserFragment.java
+++ presentation/src/main/java/com/mlsdev/githubviewer/ui/fragment/impl/SearchUserFragment.java
@@ -54,7 +54,6 @@ public class SearchUserFragment extends BaseFragment implements SearchUserView {
     @Override
     @OnClick(R.id.bt_search_user)
     public void searchUser() {
-        ((MainActivity) getActivity()).hideKeyboard();
         presenter.searchUser(etUsername.getText().toString());
     }
 
